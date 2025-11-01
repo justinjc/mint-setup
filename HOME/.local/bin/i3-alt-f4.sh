06875d2361f8bba29bc94105e802d6de11ac1cf1
Justin Chan <justin@justinjc.com>
2025-11-01 01:51:47 -0400

    i3 alt+f4 script

---
+++ HOME/.local/bin/i3-alt-f4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+WINDOW_COUNT=$(i3-msg -t get_tree | jq -r '.. | select(.focused? and .type=="workspace") | .nodes | length')
+
+if [ "$WINDOW_COUNT" -eq 0 ]; then
+    exec ~/.config/rofi/applets/bin/powermenu.sh
+fi
